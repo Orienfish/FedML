44f6eee920c1f37225d6919b9f0a0efe018a9023
QuanlingZhao <93496037+QuanlingZhao@users.noreply.github.com>
2022-06-03 08:25:56 -0700

    Add support for LSTM

---
+++ fedml_api/model/Baseline/FashionMNIST.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import copy
+
+
+class FashionMNIST_Net(nn.Module):
+    def __init__(self):
+        super(FashionMNIST_Net, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7 * 7 * 32, 10)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ fedml_api/model/Baseline/shakespeare.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import copy
+
+
+
+
+SEQ_LEN = 80
+NUM_CLASSES = 80
+NUM_HIDDEN = 100
+
+class Shakespeare_Net(nn.Module):
+    def __init__(self):
+        super(Shakespeare_Net, self).__init__()
+        self.seq_len = SEQ_LEN  # window_size?
+        self.num_classes = NUM_CLASSES  # vocab_size?
+        self.embedding_dim = 8
+        self.n_hidden = NUM_HIDDEN
+        self.embedding = nn.Embedding(self.num_classes, self.embedding_dim)
+        self.lstm = nn.LSTM(self.embedding_dim, self.n_hidden, batch_first=True)
+        self.fc = nn.Linear(self.n_hidden, self.num_classes)
+
+    def forward(self, x, prev_state):  # x: (n_samples, seq_len)
+        emb = self.embedding(x)  # (n_samples, seq_len, embedding_dim)
+        x, state = self.lstm(emb, prev_state)  # (n_samples, seq_len, n_hidden)
+        logits = self.fc(x[:, -1, :])
+        return logits, state
+
+    def zero_state(self, batch_size):
+        return (torch.zeros(1, batch_size, self.n_hidden), torch.zeros(1, batch_size, self.n_hidden))
+        
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
